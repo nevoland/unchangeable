e99969f4d85566337757da362d780ae3e86d0994
David Bonnet <david@bonnet.cc>
2024-02-17 11:14:34 -0300

    Let `isEmpty` handle any value

--- lib/tools/isEmpty.test.ts
+++ lib/tools/isEmpty.test.ts
@@ -5,9 +5,19 @@ import { isEmpty } from "./isEmpty.js";
 test("returns true for empty objects", () => {
   expect(isEmpty()).toBeTruthy();
   expect(isEmpty(null)).toBeTruthy();
+  expect(isEmpty(undefined)).toBeTruthy();
   expect(isEmpty({})).toBeTruthy();
+});
+
+test("returns true for empty arrays", () => {
+  expect(isEmpty([])).toBeTruthy();
 });
 
 test("returns false for non-empty objects", () => {
   expect(isEmpty({ a: 1 })).toBeFalsy();
+});
+
+test("returns false for non-empty arrays", () => {
+  expect(isEmpty([undefined])).toBeFalsy();
+  expect(isEmpty([1])).toBeFalsy();
 });
--- lib/tools/isEmpty.ts
+++ lib/tools/isEmpty.ts
@@ -1,18 +1,23 @@
-import { EMPTY_OBJECT } from "../constants.js";
+import { EMPTY_ARRAY, EMPTY_OBJECT } from "../constants.js";
+
+const { isArray } = Array;
 
 /**
- * Checks whether the provided object is empty or not.
+ * Checks whether the provided value is empty or not.
+ * A value is empty if it does not own any property (in case of an object) or item (in case of an array), or if it is `null` or `undefined`.
  *
- * @param value The object to check.
- * @returns `true` if the object is empty, `false` otherwise.
+ * @param value The value to check.
+ * @returns `true` if the value is empty, `false` otherwise.
  */
-export function isEmpty<T extends object = {}>(value?: T | null) {
-  if (value == null || value === EMPTY_OBJECT) {
+export function isEmpty(value?: any) {
+  if (value == null || value === EMPTY_OBJECT || value === EMPTY_ARRAY) {
     return true;
   }
+  if (isArray(value)) {
+    return value.length === 0;
+  }
   for (const name in value) {
-    // eslint-disable-next-line no-prototype-builtins
-    if (value.hasOwnProperty(name)) {
+    if (Object.prototype.hasOwnProperty.call(value, name)) {
       return false;
     }
   }
