7e2e0733662cd884e038e5bf65b215f911ac6852
David Bonnet <david@bonnet.cc>
2024-07-08 18:54:39 +0200

    Update documentation

--- lib/tools/setItem.ts
+++ lib/tools/setItem.ts
@@ -25,16 +25,65 @@ export function setItem<T>(
   index: number | undefined,
   value: T,
 ): T[];
+/**
+ * Returns a new array with the `value` prepended to it if `operation` is `PREPEND` or appended to it if `operation` is `APPEND`. If `operation` is `REMOVE`, the `value` is removed from the `array` if it exists, otherwise it returns the same `array`.
+ * If the `array` is `undefined`, it is considered as an `EMPTY_ARRAY`.
+ *
+ * @example
+ * ```typescript
+ * const result1 = setItem(["a", "b"], APPEND, "c")
+ * // ["a", "b", "c"]
+ * const result2 = setItem(["a", "b", "c"], REMOVE, "b")
+ * // ["a", "c"]
+ * ```
+ *
+ * @param array The array to update.
+ * @param operation The operation to use (PREPEND, APPEND, or REMOVE).
+ * @param value The value to insert or remove.
+ * @returns A new updated array or the same `array` if no change was necessary.
+ */
 export function setItem<T>(
   array: T[] | undefined,
   operation: Prepend | Append | Remove,
   value: T,
 ): T[];
+/**
+ * Returns a new array with the value at the given `index` removed from the `array`.
+ * If `index` is `undefined`, a negative number, or greater than `array.length`, returns the `array` untouched.
+ * If the `array` is `undefined`, it is considered as an `EMPTY_ARRAY`.
+ *
+ * @example
+ * ```typescript
+ * const result = setItem(["a", "b"], 1, REMOVE)
+ * // ["a"]
+ * ```
+ *
+ * @param array The array to update.
+ * @param index The index of the item to remove.
+ * @param operation `REMOVE`.
+ * @returns A new updated array or the same `array` if no change was necessary.
+ */
 export function setItem<T>(
   array: T[] | undefined,
   index: number | undefined,
   operation: Remove,
 ): T[];
+/**
+ * Returns a new array with the value inserted at the given `index`. If the `operation` is `PREPEND`, inserts the item before the item at the specified `index`. If the `operation` is `APPEND`, inserts the item after the item at the specified `index`.
+ * If `index` is `undefined`, a negative number, or greater than `array.length`, returns the `array` untouched.
+ * If the `array` is `undefined`, it is considered as an `EMPTY_ARRAY`.
+ *
+ * @example
+ * ```typescript
+ * const result = setItem(["a", "b"], 1, APPEND, "c")
+ * // ["a", "b", "c"]
+ * ```
+ *
+ * @param array The array to update.
+ * @param index The index where to insert the item.
+ * @param operation Either `PREPEND` or `APPEND`.
+ * @returns A new updated array or the same `array` if no change was necessary.
+ */
 export function setItem<T>(
   array: T[] | undefined,
   index: number | undefined,
